f9d1e1415f820ed16efe5865dfd978b7b89c3db6
kasp315b <gumbas.2601@gmail.com>
2019-04-17 08:38:12 +0200

    Project Upload

---
+++ TerminalEdit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TerminalEdit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        private bool running;
+        private int cursorX;
+        private int cursorY;
+
+        public Program()
+        {
+            running = true;
+            cursorX = 0;
+            cursorY = 0;
+            Run();
+        }
+
+        public void Run()
+        {
+            while(running)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                }
+                Thread.Sleep(20);
+            }
+        }
+    }
+}
---
+++ TerminalEdit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TerminalEdit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TerminalEdit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b98d508e-d016-47f6-b7f4-a01d5d3cd2ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
